77e300538481a5745d38420f09636dee692fdc51
Nils Kriedner <nkriedner@googlemail.com>
2021-02-28 10:44:49 +0100

    Create basic version

---
+++ script.js
@@ -0,0 +1,35 @@
+// Defining the ADA result variables
+
+let ada1 = 0;
+let ada500 = 0;
+let ada1006 = 0;
+
+
+// Making the Api request
+
+const req = new XMLHttpRequest();
+
+req.onload = function() {
+  console.log("ALL DONE WITH REQUEST!!!");
+  console.log(this.response);
+  const data = JSON.parse(this.response);
+  ada1 = data.ticker.price;
+  adaDisplay1.textContent = Math.floor(ada1 * 100) / 100;
+  adaDisplay500.textContent = Math.floor((ada1 * 500) * 100) / 100;
+  adaDisplay1006.textContent = Math.floor((ada1 * 1006) * 100) / 100;
+}
+
+req.onerror = function() {
+  console.log("ERROR!!!");
+  console.log(this);
+}
+
+req.open("GET", "https://api.cryptonator.com/api/ticker/ada-eur");
+req.send();
+
+
+// Printing the result to the page
+
+const adaDisplay1 = document.getElementById("ada1");
+const adaDisplay500 = document.getElementById("ada500");
+const adaDisplay1006 = document.getElementById("ada1006");
